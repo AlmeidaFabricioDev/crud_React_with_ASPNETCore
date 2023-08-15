73aefb40fc77a204d0546e5da6f66453c1355739
AlmeidaFabricioDev <devfabsalmeida@gmail.com>
2023-08-15 10:20:40 -0400

    Primeiro Commit

---
+++ front/pro-atividade-app/src/components/Atividade.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+export default function Atividade(props) {
+
+  function prioridadeLabel(param) {
+    switch (param) {
+      case 'Baixa':
+      case 'Normal':
+      case 'Alta':
+        return param;
+      default:
+        return 'Não definido';
+    }
+  }
+
+  function prioridadeStyle(param, icone) {
+    switch (param) {
+      case 'Baixa':
+        return icone ? 'smile' : 'success';
+      case 'Normal':
+        return icone ? 'meh' : 'dark';
+      case 'Alta':
+        return icone ? 'frown' : 'warning';
+      default:
+        return 'Não definido';
+    }
+  }
+  return (
+    <div className={'card mb-2 shadow-sm border-' + prioridadeStyle(props.prioridade)} >
+      <div className="card-body">
+        <div className='d-flex justify-content-between'>
+          <h5 className='card-title'>
+            <span className="badge bg-secondary me-1">{props.id}</span>
+            - {props.titulo}
+          </h5>
+          <h6>
+            Prioridade:
+            <span className={'ms-1 text-' + prioridadeStyle(props.prioridade)}>
+              <i className={'me-1 far fa-' + prioridadeStyle(props.prioridade, true)}></i>
+              {prioridadeLabel(props.prioridade)}
+            </span>
+
+
+          </h6>
+        </div>
+
+        <p className="card-text">
+          {props.descricao}
+        </p>
+        <div className='d-flex justify-content-end pt-2 m-0 border-top'>
+          <button className='btn btn-sm btn-outline-primary me-2'
+            onClick={() => props.pegarAtividade(props.id)}
+          >
+            <i className='fas fa-pen me-2'></i>
+            Editar
+          </button>
+          <button className='btn btn-sm btn-outline-danger'
+            onClick={() => props.handleConfirmModal(props.id)}
+          >
+            <i className='fas fa-trash me-2'></i>
+            Deletar
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
